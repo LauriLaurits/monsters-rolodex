f0f77e7f45de3aa0be8a9f85f6885484e0121fc9
Lauri Laurits <lauri@opus.local>
2021-07-07 09:26:41 +0300

    Test

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.styles.css';
 
 export const Card = (props) => (
     <div className='card-container'>
-    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`}></img>
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set3&size=180x180`}></img>
     <h1>{props.monster.name}</h1>
     <h2>{props.monster.email}</h2>
     </div>
